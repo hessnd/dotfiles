78906ff1cba82f649114585cafcb79918b781c81
hessnd <n.hess@bigspaceship.com>
2019-03-27 20:05:03 -0400

    rm existing config files before symlinking new ones

--- install.sh
+++ install.sh
@@ -3,12 +3,19 @@ set -e
 # Change shell to zsh
 chsh -s $(which zsh)
 
+echo -n "remove default files..."
+rm $HOME/.vimrc
+rm $HOME/.zshrc
+rm $HOME/.gitconfig
+
 echo -n "SymLinking dotfiles..."
 #Symlink .zshrc and .vimrc
 ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
+
 #mkdir $HOME/.vim/
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
+
 echo -n "Setting italics in vim"
 tic xterm-256color-italic.terminfo
 echo 'done!'
