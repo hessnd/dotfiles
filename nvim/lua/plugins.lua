1f9745d0e5eee2d5d209789fb9de296448e1d315
Nick Hess <nick.hess@onepeloton.com>
2022-11-15 15:55:09 -0700

    ensure packer is installed and bootstrapped

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,12 +1,19 @@
-local status, packer = pcall(require, "packer")
-if (not status) then
-  print("Packer is not installed")
-  return
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 vim.cmd [[packadd packer.nvim]]
 
-packer.startup(function(use)
+return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'nvim-tree/nvim-web-devicons'
 
@@ -75,4 +82,7 @@ packer.startup(function(use)
   }
   use 'norcalli/nvim-colorizer.lua'
   use 'David-Kunz/jester'
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
