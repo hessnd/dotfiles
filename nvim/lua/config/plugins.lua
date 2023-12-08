9030cda4c7dc1e691013d2b8d2bc4cc1ba30b5fb
hessnd <nick.hess@onepeloton.com>
2023-12-08 06:39:05 -0700

    update glance

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -234,18 +234,9 @@ return {
   },
   {
     "dnlhc/glance.nvim",
-    config = true,
-    opts = {
-      hooks = {
-        before_open = function(results, open, jump, method)
-          if #results == 1 then
-            jump(results[1]) -- argument is optional
-          else
-            open(results)    -- argument is optional
-          end
-        end,
-      },
-    },
+    config = function()
+      require("plugins.glance")
+    end,
     cmd = { "Glance" },
     keys = {
       { "gd", "<cmd>Glance definitions<CR>",      desc = "LSP Definition" },
