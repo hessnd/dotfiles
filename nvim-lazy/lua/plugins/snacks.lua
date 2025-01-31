6fafc2bcc8b39447e17800f2d5fe46ec93747e71
hessnd <nick.hess@onepeloton.com>
2025-01-31 09:46:10 -0700

    get snacks to show and search dotfiles

---
+++ nvim-lazy/lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {},
+    picker = {
+      enabled = true,
+      sources = {
+        grep = {
+          hidden = true,
+        },
+        files = {
+          hidden = true,
+        },
+        explorer = {
+          hidden = true,
+        },
+      },
+    },
+  },
+}
